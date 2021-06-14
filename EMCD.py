616343ea5660ca68cbbbe210ab48c38d69b2c9a9
xiaosonggege <1243049371@qq.com>
2021-06-14 16:42:12 +0800

    EMCD.py

--- EMCD.py
+++ EMCD.py
@@ -13,25 +13,55 @@ import sys
 sys.path.append('/root/PyEMD/')
 from PyEMD import EMD, Visualisation
 
-class Name:
-    pass
+class EMDblock:
+    def __init__(self, signal:np.ndarray, imfs_num:int):
+        self._signal = signal
+        self._imfs_num = imfs_num
+
+    @property
+    def signal(self):
+        return self._signal
+    @signal.setter
+    def signal(self, signal:np.ndarray):
+        self._signal = signal
+
+    @property
+    def imfs_num(self):
+        return self._imfs_num
+    @imfs_num.setter
+    def imfs_num(self, imfs_num:int):
+        self._imfs_num = imfs_num
+
+    def fenjie(self):
+        emd = EMD()
+        emd.emd(self._signal, max_imf=self._imfs_num)
+        imfs, res = emd.get_imfs_and_residue()
+        return imfs, res
+
+    def plot(self, imfs:np.ndarray, res:np.ndarray):
+        t = np.arange(self._signal.shape[0])
+        vis = Visualisation()
+        vis.plot_imfs(imfs=imfs, residue=res, t=t, include_residue=True)
+        vis.plot_instant_freq(t, imfs=imfs)
+        vis.show()
+
 
 
 if __name__ == '__main__':
-    t = np.arange(0, 3, 0.01)
-    S = np.sin(13 * t + 0.2 * t ** 1.4) - np.cos(3 * t)
-
-    # Extract imfs and residue
-    # In case of EMD
-    emd = EMD()
-    emd.emd(S)
-    imfs, res = emd.get_imfs_and_residue()
-
-    # In general:
-    # components = EEMD()(S)
-    # imfs, res = components[:-1], components[-1]
-
-    vis = Visualisation()
-    vis.plot_imfs(imfs=imfs, residue=res, t=t, include_residue=True)
-    vis.plot_instant_freq(t, imfs=imfs)
-    vis.show()
+    # t = np.arange(0, 3, 0.01)
+    # S = np.sin(13 * t + 0.2 * t ** 1.4) - np.cos(3 * t)
+    # # Extract imfs and residue
+    # # In case of EMD
+    # emd = EMD()
+    # emd.emd(S, max_imf=2)
+    # imfs, res = emd.get_imfs_and_residue()
+    # print(imfs.data, imfs.shape, imfs.__class__)
+    # print(res.data, res.shape)
+    # # In general:
+    # # components = EEMD()(S)
+    # # imfs, res = components[:-1], components[-1]
+    # vis = Visualisation()
+    # vis.plot_imfs(imfs=imfs, residue=res, t=t, include_residue=True)
+    # vis.plot_instant_freq(t, imfs=imfs)
+    # vis.show()
+    pass
